49f3bf768dc2b939693b36a6bd06e7f2aeef9e59
Vas <whiterocket@outlook.com>
2015-08-12 19:23:24 +0300

    move blog.host to etc.host because it makes more sense that way

--- etc/plain_blog.js
+++ etc/plain_blog.js
@@ -3,10 +3,10 @@ module.exports = {
 		dir: "/usr/local/share/plain_blog"
 	},
 	http: {
-		port: 80
+		port: 80,
+		host: "blog.localhost",
 	},
 	blog: {
-		host: "blog.localhost",
 		title: "Plain blog",
 		description: "Plain blog is very plain",
 		keywords: ["plain", "blog", "plain blog"],

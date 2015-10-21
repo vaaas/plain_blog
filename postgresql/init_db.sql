1dff944ef1df3f0c783aab79a871a735c2b86452
Vas <whiterocket@outlook.com>
2015-10-21 19:47:57 +0300

    yolo

---
+++ postgresql/init_db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE plain_blog;
+
+\c plain_blog
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    published DATE NOT NULL,
+    title VARCHAR(128) NOT NULL,
+    categories VARCHAR(128)[],
+    blurb TEXT NOT NULL DEFAULT '',
+    contents TEXT NOT NULL
+);

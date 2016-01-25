78125ede610623193c7eb9db688d245278bf53a2
Vas <whiterocket@outlook.com>
2016-01-25 14:26:40 +0200

    automatic semicolon insertion, better routing

--- bin/plain_blog.js
+++ bin/plain_blog.js
@@ -1,23 +1,25 @@
 #!/usr/bin/env node
-/* jshint esnext:true */
-/* jshint -W009 */
-/* jshint -W010 */
-/* jshint -W069 */
-/* jshint -W083 */
+// jshint esnext:true
+// jshint asi:true
+// jshint -W009
+// jshint -W010
+// jshint -W069
+// jshint -W083
 
-"use strict";
+"use strict"
 
 // libraries
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
-const zlib = require("zlib");
-const path = require("path");
-const dot = require("dot");
-const cheerio = require("cheerio");
+const http = require("http")
+const fs = require("fs")
+const url = require("url")
+const zlib = require("zlib")
+const path = require("path")
+const dot = require("dot")
+const cheerio = require("cheerio")
 
 // globals
-var Render, Data, Conf;
+let Render, Data, Conf
+let router
 
 const mime_types = {
 	".html": "text/html",
@@ -71,190 +73,174 @@ const mime_types = {
 	".mov": "video/quicktime",
 	".3gp": "video/3gpp",
 	".3gpp": "video/3gpp",
-};
+}
 
 // array[index] = val → object[val] = index
 function array_to_object (arr) {
-	var obj = new Object();
-	for (var i = 0, len = arr.length; i < len; i++) {
-		obj[arr[i]] = i;
-	}
-	return obj;
+	let obj = new Object()
+	for (let i = 0, len = arr.length; i < len; i++)
+		obj[arr[i]] = i
+	return obj
 }
 
 // return the mime type of a file
 function determine_mime_type (path) {
-	var index = path.slice(path.lastIndexOf("."));
-	if (index in mime_types) {
-		return mime_types [index];
-	} else {
-		return "application/octet_stream";
-	}
+	let index = path.slice(path.lastIndexOf("."))
+	if (index in mime_types) return mime_types [index]
+	else return "application/octet_stream"
 }
 
 // a blog entry
 class Post {
 	constructor (pathname) {
-		this.extract_data(pathname);
+		this.extract_data(pathname)
 	}
 
 	extract_data (pathname) {
-		this.pathname = pathname;
-		this.basename = path.basename(this.pathname);
-		this.mtime = fs.statSync(this.pathname).mtime;
+		this.pathname = pathname
+		this.basename = path.basename(this.pathname)
+		this.mtime = fs.statSync(this.pathname).mtime
 
 		// blog posts are in html, parse them and extract data from them
-		var $ = cheerio.load(fs.readFileSync(pathname, {"encoding":"utf-8"}));
-		this.title = $("h1:first-of-type").html().trim();
-		this.blurb = $("#blurb").html().trim();
-		this.content = $("body").html().trim();
+		let $ = cheerio.load(fs.readFileSync(pathname, {"encoding":"utf-8"}))
+		this.title = $("h1:first-of-type").html().trim()
+		this.blurb = $("#blurb").html().trim()
+		this.content = $("body").html().trim()
 		// the meta element has a different API because it likes to feel special
-		var meta = $("meta");
-		for (var i = 0; i < meta.length; i++) {
+		let meta = $("meta")
+		for (let i = 0; i < meta.length; i++) {
 			switch (meta[i].attribs.name) {
 			case "date":
-				this.date = new Date(meta[i].attribs.content);
-				break;
+				this.date = new Date(meta[i].attribs.content)
+				break
 			case "keywords":
-				this.categories = meta[i].attribs.content.split(", ");
-				this.categories_hash = array_to_object(this.categories);
-				break;
+				this.categories = meta[i].attribs.content.split(", ")
+				this.categories_hash = array_to_object(this.categories)
+				break
 			default:
-				break;
+				break
 			}
 		}
 	}
 
 	update () {
-		this.extract_data(this.pathname);
+		this.extract_data(this.pathname)
 	}
 
 	has_category (category) {
-		return (category in this.categories_hash);
+		return (category in this.categories_hash)
 	}
 }
 
 // a data structure holding all the blog posts & metadata
 class DB {
 	constructor (pathname) {
-		this.pathname = pathname;
-		var posts = fs.readdirSync(this.pathname);
-		this.posts = new Object();
-		for (var i = 0, len = posts.length; i < len; i++) {
-			this.posts[posts[i]] = new Post(path.join(this.pathname, posts[i]));
-		}
-		this.init_sorted();
+		this.pathname = pathname
+		let posts = fs.readdirSync(this.pathname)
+		this.posts = new Object()
+		for (let i = 0, len = posts.length; i < len; i++)
+			this.posts[posts[i]] = new Post(path.join(this.pathname, posts[i]))
+		this.init_sorted()
 	}
 
 	// inits the array of sorted posts (reverse alphabetical order)
 	init_sorted () {
-		this.sorted = Object.keys(this.posts);
-		this.sorted.sort();
-		this.sorted.reverse();
-		this.length = this.sorted.length;
-		for (var i = 0; i < this.length; i++) {
-			this.posts[this.sorted[i]].id = i;
-		}
+		this.sorted = Object.keys(this.posts)
+		this.sorted.sort()
+		this.sorted.reverse()
+		this.length = this.sorted.length
+		for (let i = 0; i < this.length; i++)
+			this.posts[this.sorted[i]].id = i
 	}
 
 	// reload the files
 	update () {
-		var posts = fs.readdirSync(path.join(this.pathname, "posts"));
-		var hash = array_to_object(posts);
-		this.purge_non_existent(hash);
-		this.add_update_new(posts);
-		this.init_sorted();
+		let posts = fs.readdirSync(path.join(this.pathname, "posts"))
+		let hash = array_to_object(posts)
+		this.purge_non_existent(hash)
+		this.add_update_new(posts)
+		this.init_sorted()
 	}
 
 	purge_non_existent (existent) {
 		// remove files that no longer exist from the database
-		for (var key in this.posts) {
-			if (this.posts.hasOwnProperty(key) && !(key in existent)) {
-				delete this.posts[key];
-			}
-		}
+		for (let key in this.posts)
+			if (this.posts.hasOwnProperty(key) && !(key in existent))
+				delete this.posts[key]
 	}
 
 	add_update_new (posts) {
 		// add files that don't exist in the database to the database
 		// additionally, update files that were modified
-		var pathname = "";
-		for (var i = 0, len = posts.length; i < len; i++) {
-			pathname = path.join(this.pathname, "posts", posts[i]);
-			if (!(this.exists(posts[i]))) {
-				this.posts[posts[i]] = new Post(pathname);
-			} else if (fs.statSync(pathname).mtime > this.posts[posts[i]].mtime) {
-				this.posts[posts[i]].update();
-			}
+		let pathname = ""
+		for (let i = 0, len = posts.length; i < len; i++) {
+			pathname = path.join(this.pathname, "posts", posts[i])
+			if (!(this.exists(posts[i])))
+				this.posts[posts[i]] = new Post(pathname)
+			else if (fs.statSync(pathname).mtime > this.posts[posts[i]].mtime)
+				this.posts[posts[i]].update()
 		}
 	}
 
 	// run a query on the data
 	// q: a URI query
 	query (q, callback) {
-		var results = new Array();
-		var counter = Conf.blog.posts_per_page;
-		var i = 0;
-		var step = 1;
-		var cpost;
+		let results = new Array()
+		let counter = Conf.blog.posts_per_page
+		let i = 0
+		let step = 1
+		let cpost
 
 		if (q.newer && q.older) {
-			q.newer = null;
-			q.older = null;
+			q.newer = null
+			q.older = null
 		} else if (q.newer) {
-			q.newer = parseInt(q.newer, 10);
+			q.newer = parseInt(q.newer, 10)
 			if (Number.isInteger(q.newer)) {
-				i = q.newer - 1;
-				step = -1;
-			} else {
-				q.newer = null;
-			}
+				i = q.newer - 1
+				step = -1
+			} else q.newer = null
 		} else if (q.older) {
-			q.older = parseInt (q.older, 10);
-			if (Number.isInteger(q.older)) {
-				i = q.older + 1;
-			} else {
-				q.older = null;
-			}
+			q.older = parseInt (q.older, 10)
+			if (Number.isInteger(q.older)) i = q.older + 1
+			else q.older = null
 		}
 
-		if (q.category) {
+		if (q.category)
 			for (; i >= 0 && i < this.length && counter > 0; i += step) {
-				cpost = this.posts[this.sorted[i]];
+				cpost = this.posts[this.sorted[i]]
 				if (cpost.has_category(q.category)) {
-					results.push(cpost);
-					counter -= 1;
+					results.push(cpost)
+					counter -= 1
 				}
 			}
-		} else {
+		else
 			for (; i >= 0 && i < this.length && counter > 0; i += step) {
-				cpost = this.posts[this.sorted[i]];
-				results.push(cpost);
-				counter -= 1;
+				cpost = this.posts[this.sorted[i]]
+				results.push(cpost)
+				counter -= 1
 			}
-		}
-		if (step === -1) {
-			results.reverse();
-		}
-		callback(null, results);
+		if (step === -1)
+			results.reverse()
+		callback(null, results)
 	}
 
 	// return whether post exists in database
 	exists (post) {
-		return (post in this.posts);
+		return (post in this.posts)
 	}
 
 	// get post from database
 	get (post) {
-		return (this.posts[post]);
+		return (this.posts[post])
 	}
 }
 
 class ResponseConf {
 	constructor (code, message, data) {
-		this.code = code;
-		this.message = message;
-		this.data = data;
+		this.code = code
+		this.message = message
+		this.data = data
 	}
 
 	static code (num, msg) {
@@ -262,7 +248,7 @@ class ResponseConf {
 			num,
 			{"Content-type": "text/plain"},
 			msg ? msg : "" + num
-		);
+		)
 	}
 
 	static rss (results) {
@@ -274,7 +260,7 @@ class ResponseConf {
 				host: Conf.http.host,
 				posts: results
 			})
-		);
+		)
 	}
 
 	static file (pathname) {
@@ -282,7 +268,7 @@ class ResponseConf {
 			200,
 			{"Content-type": determine_mime_type(pathname)},
 			fs.createReadStream(pathname)
-		);
+		)
 	}
 
 	static post (name) {
@@ -294,7 +280,7 @@ class ResponseConf {
 				type: "element",
 				post: Data.get(name)
 			})
-		);
+		)
 	}
 
 	static post_list (results, query) {
@@ -307,7 +293,7 @@ class ResponseConf {
 				category: query.category || null,
 				posts: results
 			})
-		);
+		)
 	}
 
 	static empty_page () {
@@ -318,7 +304,82 @@ class ResponseConf {
 				blog: Conf.blog,
 				type: "empty"
 			})
-		);
+		)
+	}
+}
+
+class Router {
+	constructor () {
+		this.routes = []
+	}
+
+	indexOf (route) {
+		for (var i = 0, len = this.routes.length; i < len; i++)
+			if (this.routes[i].match === route) return i
+		return -1
+	}
+
+	add_route (method, route, handler) {
+		route = route.split("/").slice(1)
+		if (route[0] === "") route = ["root"]
+		let i = this.indexOf(route)
+		if (i >= 0) this.routes[i][method] = handler
+		else {
+			let obj = {}
+			obj["match"] = route
+			obj[method] = handler
+			this.routes.push(obj)
+		}
+	}
+
+	match (method, str) {
+		str = str.split("/").slice(1)
+		if (str[0] === "") str = ["root"]
+		for (let i = 0, len = this.routes.length; i < len; i++) {
+			let match = this.match_route (str, method, this.routes[i])
+			if (match === null) continue
+			else return match
+		}
+		return null
+	}
+
+	match_route (str, method, route) {
+		let match = route.match
+		let params = []
+		let i = 0
+		let j = 0
+		while (i < str.length) {
+			switch(match[j]) {
+			case str[i]:
+				i++
+				j++
+				break
+			case "*":
+				params.push(str[i])
+				i++
+				j++		
+				break
+			case "**":
+				if (match[j+1]) {
+					let ind = str.indexOf(match[j+1], i)
+					if (ind === -1) return null
+					params.push(str.slice(i, ind).join("/"))
+					i = ind
+					j++
+				} else {
+					params.push(str.slice(i).join("/"))
+					i = str.length
+				}
+				break
+			default:
+				return null
+				break
+			}
+		}
+		if (route.hasOwnProperty(method))
+			return [route[method], params]
+		else
+			return null
 	}
 }
 
@@ -326,81 +387,71 @@ class ResponseConf {
 // conf: a response configuration object
 function serve (res, conf) {
 	// almost everything supports gzip compressed responses nowadays
-	var gzip = zlib.createGzip();
-	conf.message["content-encoding"] = "gzip";
+	let gzip = zlib.createGzip()
+	conf.message["content-encoding"] = "gzip"
 
 	// we accept both streams and raw data
-	res.writeHead(conf.code, conf.message, conf.headers);
+	res.writeHead(conf.code, conf.message, conf.headers)
 	if (conf.data.constructor === fs.ReadStream) {
-		conf.data.pipe(gzip).pipe(res);
+		conf.data.pipe(gzip).pipe(res)
 	} else {
-		gzip.end(conf.data);
-		gzip.pipe(res);
+		gzip.end(conf.data)
+		gzip.pipe(res)
 	}
 }
 
 function request_listener (req, res) {
-	function DRY (conf) { serve(res, conf); }
-	req.url = url.parse(req.url, true);
-	req.url.basename = path.basename(req.url.pathname);
-
-	if (req.method !== "GET") {
-		return DRY(ResponseConf.code(405));
-	} else if (req.url.pathname === "/") {
-		get_posts_collection({}, DRY);
-	} else if (req.url.pathname === "/posts") {
-		get_posts_collection(req.url.query, DRY);
-	} else if (req.url.pathname.startsWith("/posts/")) {
-		get_posts_element(req.url.basename, DRY);
-	} else if (req.url.pathname.startsWith("/static/")) {
-		get_static_element(req.url.pathname, DRY);
-	} else if (req.url.pathname === "/feeds/rss.xml") {
-		get_rss_feed(DRY);
-	} else {
-		return DRY(ResponseConf.code(404));
-	}
+	function DRY (conf) { serve(res, conf) }
+	req.url = url.parse(req.url, true)
+	req.url.basename = path.basename(req.url.pathname)
+	let match = router.match(req.method, req.url.pathname)
+	if (match === null)
+		DRY(ResponseConf.code(404))
+	else
+		match[0](req, ...match[1], DRY)
 }
 
-function get_posts_collection (query, callback) {
+function get_posts_collection (req, callback) {
+	let query = req.url.query
 	Data.query(query, function (err, results) {
 		if (err) {
-			callback(ResponseConf.code (500, err.message));
+			callback(ResponseConf.code (500, err.message))
 		} else if (results.length > 0) {
-			callback(ResponseConf.post_list(results, query));
+			callback(ResponseConf.post_list(results, query))
 		} else {
 			// nothing found
-			return callback(ResponseConf.empty_page());
+			return callback(ResponseConf.empty_page())
 		}
-	});
+	})
 }
 
-function get_posts_element (name, callback) {
+function get_posts_element (req, name, callback) {
 	if (Data.exists(name)) {
-		callback(ResponseConf.post(name));
+		callback(ResponseConf.post(name))
 	} else {
-		callback(ResponseConf.empty_page());
+		callback(ResponseConf.empty_page())
 	}
 }
 
-function get_static_element (what, callback) {
-	var pathname = path.join(Conf.fs.dir, what);
+function get_static_element (req, what, callback) {
+	let pathname = path.join(Conf.fs.dir, "static", what)
 	fs.exists(pathname, function (exists) {
 		if (!exists) {
-			return callback(ResponseConf.code(404, "Element doesn't exist"));
+			return callback(ResponseConf.code(404, "Element doesn't exist"))
 		} else {
-			return callback(ResponseConf.file(pathname));
+			return callback(ResponseConf.file(pathname))
 		}
-	});
+	})
 }
 
-function get_rss_feed (callback) {
+function get_rss_feed (req, callback) {
 	Data.query({}, function (err, results) {
 		if (results.length === 0) {
-			callback(ResponseConf.code(404));
+			callback(ResponseConf.code(404))
 		} else {
-			callback(ResponseConf.rss(results));
+			callback(ResponseConf.rss(results))
 		}
-	});
+	})
 }
 
 function read_env_conf () {
@@ -419,13 +470,13 @@ function read_env_conf () {
 			author: process.env.AUTHOR || "User didn't configure blog author",
 			posts_per_page: process.env.PPP || 10,
 		}
-	};
+	}
 }
 
 function HUP_listener () {
-	read_env_conf();
-	init_templates();
-	Data.update();
+	read_env_conf()
+	init_templates()
+	Data.update()
 }
 
 function init_templates () {
@@ -442,40 +493,46 @@ function init_templates () {
 				{"encoding": "utf-8"}
 			)
 		)
-	};
+	}
 }
 
 function init_server () {
-	var server = http.createServer(request_listener);
-	server.listen(Conf.http.port, Conf.http.host);
+	const server = http.createServer(request_listener)
+	router = new Router()
+	router.add_route("/", "GET", get_posts_collection)
+	router.add_route("/posts", "GET", get_posts_collection)
+	router.add_route("/posts/*", "GET", get_posts_element)
+	router.add_route("/static/**", "GET", get_static_element)
+	router.add_route("/feeds/rss.xml", "GET", get_rss_feed)
+	server.listen(Conf.http.port, Conf.http.host)
 }
 
 function main () {
-	process.on("SIGHUP", HUP_listener);
-	read_env_conf();
+	process.on("SIGHUP", HUP_listener)
+	read_env_conf()
 	try {
-		Data = new DB(path.join(Conf.fs.dir, "/posts"));
+		Data = new DB(path.join(Conf.fs.dir, "/posts"))
 	} catch (e) {
-		console.error("Couldn't initialise databse", e.name, e.message);
-		throw new Error();
+		console.error("Couldn't initialise databse", e.name, e.message)
+		throw new Error()
 	}
 	try {
-		init_templates();
+		init_templates()
 	} catch (e) {
-		console.error("Couldn't initialise templates", e.name, e.message);
-		throw new Error();
+		console.error("Couldn't initialise templates", e.name, e.message)
+		throw new Error()
 	}
 	try {
-		init_server();
+		init_server()
 	} catch (e) {
-		console.error("Couldn't start server", e.name, e.message);
-		throw new Error();
+		console.error("Couldn't start server", e.name, e.message)
+		throw new Error()
 	}
-	console.log(`Server listening to ${Conf.http.host}:${Conf.http.port}`);
+	console.log(`Server listening to ${Conf.http.host}:${Conf.http.port}`)
 }
 
 try {
-	main();
+	main()
 } catch (e) {
-	process.exit(1);
+	process.exit(1)
 }

d9077b32305efc603a2abe14dba3cf2da6540167
Vas <whiterocket@outlook.com>
2016-06-09 19:33:00 +0300

    determine-mime-type

--- plain_blog.js
+++ plain_blog.js
@@ -11,60 +11,7 @@ const path = require("path")
 const dot = require("dot")
 const cheerio = require("cheerio")
 const regex-router = require("regex-router")
-
-const mime_types = {
-	".html": "text/html",
-	".htm": "text/html",
-	".css": "text/css",
-	".xml": "text/xml",
-	".txt": "text/plain",
-
-	".gif": "image/gif",
-	".jpeg": "image/jpeg",
-	".jpg": "image/jpeg",
-	".png": "image/png",
-	".ico": "image/x-icon",
-	".bmp": "image/x-ms-bmp",
-	".svg": "image/svg+xml",
-	".svgz": "image/svg+xml",
-	".webp": "image/webp",
-
-	".js": "application/javascript",
-	".atom": "application/atom+xml",
-	".rss": "application/rss+xml",
-	".json": "application/json",
-	".woff": "application/font-woff",
-	".jar": "application/java-archive",
-	".war": "applicaiton/java-archive",
-	".ear": "applicaiton/java-archive",
-	".doc": "application/msword",
-	".pdf": "application/pdf",
-	".rtf": "application/rtf",
-	".xls": "application/vnd.ms-excel",
-	".ppt": "application/vnd.ms-powerpoint",
-	".xhtml": "application/xhtml+xml",
-	".7z": "application/x-7z-compressed",
-	".zip": "application/zip",
-	".rar": "application/x-rar-compressed",
-
-	".mp3": "audio/mpeg",
-	".ogg": "audio/ogg",
-	".oga": "audio/ogg",
-	".m4a": "audio/x-m4a",
-	".aac": "audio/x-m4a",
-
-	".webm": "video/webm",
-	".mp4": "video/mp4",
-	".mkv": "video/x-matroska",
-	".flv": "video/x-flv",
-	".avi": "video/x-msvideo",
-	".mpg": "video/mpeg",
-	".mpeg": "video/mpeg",
-	".wmv": "video/x-ms-wmv",
-	".mov": "video/quicktime",
-	".3gp": "video/3gpp",
-	".3gpp": "video/3gpp",
-}
+const determine-mime-type = require("determine-mime-type")
 
 function array_to_object (arr) {
 	let obj = {}
@@ -72,11 +19,6 @@ function array_to_object (arr) {
 		obj[arr[i]] = i
 	return obj
 } 
-
-function determine_mime_type (path) {
-	let index = path.slice(path.lastIndexOf("."))
-	return mime_types[index] || "application/octet_stream"
-}
 
 class ResponseConf {
 	constructor (code, headers, data) {
@@ -222,7 +164,7 @@ class View {
 	file (pathname) {
 		return new ResponseConf(
 			200,
-			{"Content-type": determine_mime_type(pathname)},
+			{"Content-type": determine-mime-type(pathname)},
 			fs.createReadStream(pathname)
 		)
 	}

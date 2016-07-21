c070ecea7541e57f34412b9118f2b5a4e2f48c5e
Vas <whiterocket@outlook.com>
2016-07-21 16:29:48 +0300

    separate concerns

--- plain_blog.js
+++ plain_blog.js
@@ -1,29 +1,16 @@
 #!/usr/bin/env node
+// jshint node:true
 // jshint esnext:true
 // jshint asi:true
 
 "use strict"
 
-const dot = require("dot")
 const Conf = require("./lib/Conf.js")
-const Controller = require("./lib/Controller.js")
-const Model = require("./lib/Model.js")
-const View = require("./lib/View.js")
+const WebServer = require("./lib/WebServer.js")
 
 function main () {
 	const conf = new Conf()
-	const model = new Model("./posts", conf.blog.posts_per_page)
-	const view = new View(
-		dot.process({path: "./templates"}),
-		conf.http.host,
-		conf.blog
-	)
-	const controller = new Controller (
-		model,
-		view,
-		conf.http.port,
-		conf.http.host
-	)
+	WebServer(conf)
 	console.log(`Server listening to ${conf.http.host}:${conf.http.port}`)
 }
 
